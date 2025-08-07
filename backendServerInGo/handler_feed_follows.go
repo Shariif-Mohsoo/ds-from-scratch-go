27b800f26a9a17bb7956aeb31e94c21ef47a47ce
Shariif-Mohsoo <mr8781256@gmail.com>
2025-08-07 11:35:57 +0500

    Done With Feeds

---
+++ backendServerInGo/handler_feed_follows.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"encoding/json" // For decoding JSON data from the request body
+	"fmt"           // For formatting error messages
+	"net/http"      // For handling HTTP requests and responses
+	"time"          // For working with timestamps
+
+	"github.com/Shariif-Mohsoo/ds-from-scratch-go/rssagg/internal/database" // Your internal database package
+	"github.com/google/uuid"                                                // For generating unique user IDs
+)
+
+// This is a method of the `apiConfig` struct
+// It handles feed creation when a request is made to the feed creation endpoint
+
+// Parameters:
+// - w: http.ResponseWriter → sends the response back to the client (like a browser or API tool)
+// - r: *http.Request → represents the incoming HTTP request that contains user data
+func (apiCfg *apiConfig) handlerCreateFeedFollow(w http.ResponseWriter, r *http.Request, user database.User) {
+	// Define a struct to read JSON input from the request body
+	// It expects a JSON object like: { "name": "Your Name" }
+	type parameters struct {
+		FeedID uuid.UUID `json:"feed_id"`
+	}
+
+	// Create a new JSON decoder to read the request body
+	decoder := json.NewDecoder(r.Body)
+
+	// Create an empty `parameters` struct to hold the decoded data
+	params := parameters{}
+
+	// Try to decode the JSON body into the `params` struct
+	err := decoder.Decode(&params)
+	if err != nil {
+		// If decoding fails, return a 400 Bad Request error with details
+		respondWithError(w, 400, fmt.Sprintf("Error Parsing JSON: %v", err))
+		return
+	}
+
+	// Call the CreateUser function from your database package to save the new user
+	// Passing:
+	// - a new unique ID for the user
+	// - current time for CreatedAt and UpdatedAt
+	// - the name received from the request
+	feed, err := apiCfg.DB.CreateFeedFollow(r.Context(), database.CreateFeedFollowParams{
+		ID:        uuid.New(),       // Generate a new unique ID
+		CreatedAt: time.Now().UTC(), // Set current UTC time for when user was created
+		UpdatedAt: time.Now().UTC(), // Set current UTC time for when user was updated
+		UserID:    user.ID,
+		FeedID:    params.FeedID,
+	})
+
+	if err != nil {
+		// If something went wrong while creating the user in DB, return an error
+		respondWithError(w, 400, fmt.Sprintf("Couldn't create feed follow: %v", err))
+		return
+	}
+
+	// If user creation was successful, return the user data as JSON with 200 OK status
+	respondWithJSON(w, 201, databaseFeedToFeed(feed))
+}
+
+// Parameters:
+// - w: http.ResponseWriter → sends the response back to the client (like a browser or API tool)
+// - r: *http.Request → represents the incoming HTTP request that contains user data
+func (apiCfg *apiConfig) handlerGetFeeds(w http.ResponseWriter, r *http.Request) {
+	// Call the CreateUser function from your database package to save the new user
+	// Passing:
+	// - a new unique ID for the user
+	// - current time for CreatedAt and UpdatedAt
+	// - the name received from the request
+	feeds, err := apiCfg.DB.GetFeeds(r.Context())
+	if err != nil {
+		// If something went wrong while creating the user in DB, return an error
+		respondWithError(w, 400, fmt.Sprintf("Couldn't get feeds: %v", err))
+		return
+	}
+
+	// If user creation was successful, return the user data as JSON with 200 OK status
+	respondWithJSON(w, 200, databaseFeedsToFeeds(feeds))
+}
--- backendServerInGo/sql/schema/004_feed_follows.sql
+++ backendServerInGo/sql/schema/004_feed_follows.sql
@@ -1,13 +1,13 @@
 -- +goose up
-create table feeds(
+create table feed_follows(
     id uuid primary key,
     created_at timestamp not null,
     updated_at timestamp not null,
-    name text not null,
-    url text unique not null,
-    user_id uuid not null references users(id) on delete cascade
+    user_id uuid not null references users(id) on delete cascade,
+    feed_id uuid not null references feeds(id) on delete cascade,
+    unique(user_id, feed_id)
 );
 -- +goose down
-drop table feeds;
+drop table feed_follows;
 -- in terminal=> goose postgres postgres://postgres:mohsoo@localhost:5432/rssagg up
 -- in terminal=> goose postgres postgres://postgres:mohsoo@localhost:5432/rssagg down
